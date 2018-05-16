5a6b84f6b6e8a6b5242f167f3ce8954be24cc3fa
Hasan Naseem <hn92@live.com>
2018-05-15 21:39:22 -0400

    bug fixed

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -15,20 +15,33 @@ game.addEventListener('click', function(event) {
       checkWin(x, 'Red')
       x = 0
     }
-    console.log(score);
+    // console.log(score);
 });
 
+// Checks for one combination that was causing bug in the game
 
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
+}
+
+// check for the winning combinations using sums of all        possible outcomes
 function checkWin(x, color) {
 
-  var win = [6, 15, 12, 18, 24]
+  var win = [6, 15, 18, 24]
   score[x].push(Number(event.srcElement.id));
 
   if (score[x].length === 3) {
     var sum = score[x].reduce(function(acc, val) { return acc + val; });
 
     for (var j = 0; j < win.length; j++) {
-      if (sum === win[j]) {
+      if (sum === win[j] || arraysEqual(score[x], [1, 4, 7])) {
         document.querySelector('body').innerText = color + ' Wins!'
         }
       }
